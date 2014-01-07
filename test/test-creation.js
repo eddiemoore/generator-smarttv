589276aebf0a6f86f7ef518dfce6308c20dd7560
Ed Moore <eddie.moore@gmail.com>
2014-01-07 22:34:32 +0800

    modified test-creation

--- test/test-creation.js
+++ test/test-creation.js
@@ -13,26 +13,86 @@ describe('smarttv generator', function () {
             }
 
             this.app = helpers.createGenerator('smarttv:app', [
-                '../../app'
+                '../../app', [
+                  helpers.createDummyGenerator(),
+                  'mocha:app'
+                ]
             ]);
             done();
         }.bind(this));
     });
 
+    it('the generator can be required without throwing', function () {
+        // not testing the actual run of generators yet
+        this.app = require('../app');
+    });
+
+
     it('creates expected files', function (done) {
         var expected = [
-            // add files you expect to exist here.
-            '.jshintrc',
-            '.editorconfig'
+          ['bower.json', /"name": "temp"/],
+          ['package.json', /"name": "temp"/],
+          ['Gruntfile.js', /coffee:/],
+          'app/index.html',
+          'app/config.xml',
+          'app/scripts/main.coffee',
+          'app/styles/main.scss'
         ];
 
         helpers.mockPrompt(this.app, {
-            'someOption': true
+            features: ['includeCompass']
         });
+        
+        this.app.coffee = true;
         this.app.options['skip-install'] = true;
         this.app.run({}, function () {
             helpers.assertFiles(expected);
             done();
         });
     });
+
+    it('creates expected files in non-AMD non-coffee mode', function (done) {
+        var expected = [
+          ['bower.json', /"name": "temp"/],
+          ['package.json', /"name": "temp"/],
+          'Gruntfile.js',
+          'app/index.html',
+          'app/config.xml',
+          'app/scripts/main.js',
+          'app/styles/main.scss'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          features: ['includeCompass']
+        });
+
+        this.app.coffee = false;
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFiles(expected);
+          done();
+        });
+    });
+
+      it('creates expected files in AMD mode', function (done) {
+        var expected= [
+          ['bower.json', /"name": "temp"/],
+          ['package.json', /"name": "temp"/],
+          'Gruntfile.js',
+          'app/index.html',
+          'app/config.xml',
+          'app/scripts/main.js',
+          'app/styles/main.scss'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          features: ['includeCompass']
+        });
+
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFiles(expected);
+          done();
+        });
+      });
 });
